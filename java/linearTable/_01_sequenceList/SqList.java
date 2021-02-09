3ec339c53f604129d81bf9efb205090f3176704a
happysmile <812024380@qq.com>
2021-02-09 10:32:38 +0800

    按照这种风格的话不需要写多余的get,set方法

--- java/linearTable/_01_sequenceList/SqList.java
+++ java/linearTable/_01_sequenceList/SqList.java
@@ -1,8 +1,6 @@
 package linearTable._01_sequenceList;
 
 import linearTable.BasicOperation;
-import lombok.Getter;
-import lombok.Setter;
 
 
 public class SqList<E> implements BasicOperation<SqList<E>,E> {
@@ -10,15 +8,11 @@ public class SqList<E> implements BasicOperation<SqList<E>,E> {
     //顺序表是顺序存储的存储结构，因此它的存储方式也是顺序的。
     //而要保证它的顺序，则需要用数组来约束它。
     //下面的字段就规定了这个数组的最大大小，同时也是顺序表的最大大小
-    @Getter
     private int MaxSize = 0;
 
     //线性表的当前长度
-    @Getter
-    @Setter
     private int length = 0;
 
-    @Getter
     private Object[] data;
 
     public<E> SqList(int maxSize){
@@ -38,7 +32,7 @@ public class SqList<E> implements BasicOperation<SqList<E>,E> {
 
     @Override
     public int Length(SqList<E> l) {
-        return 0;
+        return l.length;
     }
 
     @Override

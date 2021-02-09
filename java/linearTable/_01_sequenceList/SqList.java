2ee2fffd2aea2242004d9663bfae89bcc464b785
happysmile <812024380@qq.com>
2021-02-09 14:53:12 +0800

    第1题

--- java/linearTable/_01_sequenceList/SqList.java
+++ java/linearTable/_01_sequenceList/SqList.java
@@ -112,11 +112,15 @@ public class SqList<E> implements BasicOperation<SqList<E>,E> {
     }
 
     public static void main(String[] args) {
-        SqList<Integer> sqList = new SqList<>(2);
+        SqList<Integer> sqList = new SqList<>(100);
         sqList.InitList(sqList);
         sqList.ListInsert(sqList,1,1);
-        sqList.ListInsert(sqList,2,2);
+        sqList.ListInsert(sqList,2,4);
         sqList.ListInsert(sqList,3,3);
+        sqList.ListInsert(sqList,4,2);
+        sqList.ListInsert(sqList,5,5);
+        sqList.ListInsert(sqList,6,6);
+        sqList.PrintList(sqList);
     }
 
 
---
+++ java/linearTable/_01_sequenceList/_1_.java
@@ -0,0 +1,19 @@
+package linearTable._01_sequenceList;
+
+public class _1_ {
+    public static void main(String[] args) {
+        //思路1
+        //1，遍历所有元素，找出最小的元素和它的索引
+        //2，调用根据位置删除的方法
+        //O(n) + O(n)
+
+        //思路2
+        //1,采用一种高效的策略将线性表进行排序
+        //2，调用根据位置删除的方法
+        //O(排序算法的时间复杂度) + O(n)
+
+
+
+
+    }
+}
